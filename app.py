4d9bc669fd13a46ba1e6c8b532d31429d6247a28
Vinodkumar-yerraballi <vinodkumar9676150@gmail.com>
2022-10-25 13:03:36 +0530

    update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
+import tensorflow.compat.v2 as tf
 import pandas_datareader as data
 from keras.models import load_model 
 from sklearn.preprocessing import MinMaxScaler
